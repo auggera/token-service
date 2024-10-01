9e49f482ae6b4e76077a3c44ac1e5d03e92f8e5f
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-01 19:58:20 +0200

    Initialized the token repository.

---
+++ src/main/java/ua/lastbite/token_service/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package ua.lastbite.token_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.lastbite.token_service.model.Token;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByTokenValue(String token);
+}
