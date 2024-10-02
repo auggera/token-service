c664cab0f18659465d40d0ac7f2beee5b8336571
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-02 20:16:57 +0200

    Created unit test for TokenMapper

---
+++ src/test/java/ua/lastbite/token_service/mapper/TokenMapperTest.java
@@ -0,0 +1,27 @@
+package ua.lastbite.token_service.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import ua.lastbite.token_service.dto.TokenRequest;
+import ua.lastbite.token_service.model.Token;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TokenMapperTest {
+
+    private final TokenMapper tokenMapper = Mappers.getMapper(TokenMapper.class);
+
+    @Test
+    public void testTokenMapper() {
+        TokenRequest tokenRequest = new TokenRequest(1);
+
+        Token token = tokenMapper.toEntity(tokenRequest, 86_400L);
+
+        assertNotNull(token);
+        assertEquals(1, token.getUserId());
+        assertNotNull(token.getCreatedAt());
+        assertNotNull(token.getExpiresAt());
+        assertFalse(token.isUsed());
+        assertTrue(token.getExpiresAt().isAfter(token.getCreatedAt()));
+    }
+}
