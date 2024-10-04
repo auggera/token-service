a18f7efbf9bf512f903ac6c2700c6587e40da791
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-04 18:38:43 +0200

    Improoved methods adding logs and handling exceptions

--- src/main/java/ua/lastbite/token_service/service/UserServiceClient.java
+++ src/main/java/ua/lastbite/token_service/service/UserServiceClient.java
@@ -2,12 +2,18 @@ package ua.lastbite.token_service.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import ua.lastbite.token_service.dto.user.UserDto;
+import ua.lastbite.token_service.exception.UserNotFoundException;
 
 @Service
 public class UserServiceClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceClient.class);
 
     private final RestTemplate restTemplate;
 
@@ -20,6 +26,15 @@ public class UserServiceClient {
 
     public UserDto getUserById(Integer id) {
         String url = userServiceUrl + "/users/" + id;
-        return restTemplate.getForObject(url, UserDto.class);
+        try {
+            LOGGER.info("Requesting user with ID: {}", id);
+            return restTemplate.getForObject(url, UserDto.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            LOGGER.error("User not found with ID: {}", id);
+            throw new UserNotFoundException(id);
+        } catch (RestClientException e) {
+            LOGGER.error("Error occurred while calling user-service for ID: {}", id, e);
+            throw new RuntimeException("Failed to communicate with user-service");
+        }
     }
 }
