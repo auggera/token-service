5f28be35b9fd3509f6199387fefb0a0733e0bd01
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-09 12:58:45 +0200

    Updated and improved the format validation logic also splitting the responsibility for null validation to follow the principle of delegating responsibility.

--- src/main/java/ua/lastbite/token_service/validation/TokenFormatValidator.java
+++ src/main/java/ua/lastbite/token_service/validation/TokenFormatValidator.java
@@ -5,8 +5,14 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class TokenFormatValidator implements ConstraintValidator<ValidTokenFormat, String> {
 
+    private static final String TOKEN_REGEX = "^[a-zA-Z0-9_-]+$";
+
     @Override
-    public boolean isValid(String token, ConstraintValidatorContext context) {
-        return token != null && token.matches("[A-Za-z0-9_-]+");
+    public boolean isValid(String tokenValue, ConstraintValidatorContext context) {
+        if (tokenValue == null || tokenValue.trim().isEmpty()) {
+            return true; // Let @NotBlank handle the case
+        };
+
+        return tokenValue.matches(TOKEN_REGEX);
     }
 }
