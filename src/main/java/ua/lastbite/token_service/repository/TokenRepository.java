3b8bd3cc964c5337dd4222a5fe19ca14eaa49622
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-09 17:16:38 +0200

    Created a method to delete expired and used tokens in TokenRepository.

--- src/main/java/ua/lastbite/token_service/repository/TokenRepository.java
+++ src/main/java/ua/lastbite/token_service/repository/TokenRepository.java
@@ -1,10 +1,18 @@
 package ua.lastbite.token_service.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import ua.lastbite.token_service.model.Token;
 
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByTokenValue(String tokenValue);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Token t WHERE t.expiresAt < CURRENT_TIMESTAMP OR t.used = true")
+    void deleteExpiredOrUsedTokens();
 }
